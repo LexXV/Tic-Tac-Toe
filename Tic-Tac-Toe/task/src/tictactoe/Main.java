a3733dbaa371a81f92d23f9772d73599f0c72df0
Alex Voloshin <34489048+LexXV@users.noreply.github.com>
2020-08-07 20:33:35 +0300

    Add solved part 2/5 of the project

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,9 +1,18 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter cells: ");
+        char[] array = scanner.nextLine().toCharArray();
+
+        System.out.println("---------");
+        System.out.println("| " + array[0] + " " + array[1] + " " + array[2] + " |");
+        System.out.println("| " + array[3] + " " + array[4] + " " + array[5] + " |");
+        System.out.println("| " + array[6] + " " + array[7] + " " + array[8] + " |");
+        System.out.println("---------");
     }
 }
